057694477c7e7e2752ed317b70649f8ab25ed06f
Sandro Gomes <sandro.gomes3112@gmail.com>
2023-07-30 13:00:13 -0300

    Tratando erros com exceções personalizadas

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,14 +8,22 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
-            
-            tab.ColocarPeca(new Torre(tab, Cor.Amarela), new Posicao(0, 0));
-            tab.ColocarPeca(new Torre(tab, Cor.Amarela), new Posicao(1, 3));
-            tab.ColocarPeca(new Rei(tab,Cor.Amarela), new Posicao(2, 4));
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Tela.imprimirTabuleiro(tab);
+                tab.ColocarPeca(new Torre(tab, Cor.Amarela), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Amarela), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Amarela), new Posicao(0, 9));
 
+                Tela.imprimirTabuleiro(tab);
+
+               
+            }
+            catch(TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
